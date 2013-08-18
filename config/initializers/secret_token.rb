afdc1e1ef7ea2f2674e281028b69856ff20b4051
Paritosh Piplewar <paritosh@passion8.co.in>
2013-08-19 02:02:53 +0530

    initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnockoutRailsDeviseExample::Application.config.secret_token = 'ba81dc04b163e10aa6ee5a4d483f1b8946e963956b7e49977de5cde1a55ff1ffd71a4320a935f7390c94d2b6a8b7748ce64defe54a680f28038fa14bdffbcb21'
