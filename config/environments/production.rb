3a4ef5e5033569cabfd0a5bdc99094e7dbbe8097
Paritosh Piplewar <paritosh@passion8.co.in>
2013-08-22 01:16:52 +0530

    heroku fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ KnockoutRailsDeviseExample::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => 'frozen-beach-7829.herokuapp.com/' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
